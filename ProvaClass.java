a03dd5db5151b906e86ac74ec6020ec532a3ded0
Paolo Celani <pcelani03@gmail.com>
2020-11-11 15:20:06 +0000

    Initial commit

---
+++ ProvaClass.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+    
+		int x=0,y=0;
+		for(int i=0; i<=20;i++){
+			System.out.println("x:"+x+"y:"+y);
+			x+=1;//x=x+1
+			y+=1;//Y=y+1
+		}
+
+		int a []=new int [21];
+		int b []=new int [21];
+
+		
+
+
+  }
+}
